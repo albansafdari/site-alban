914400176681fae8ab37a01227485d7392a8f674
albansafdari <73873989+albansafdari@users.noreply.github.com>
2020-11-30 17:30:25 +0100

    Update apropos.php

--- apropos.php
+++ apropos.php
@@ -1,7 +1,7 @@
 <div id="APropos">
     <h1>A Propos</h1>
 
-    <p>Après un bac STI2D EE (Énergie Environnement) obtenu avec mention, je me suis orienté vers le domaine de l'informatique. J'ai donc fait une première année de FAC en informatique, voyant qu'une partie des matières ne me plaisait pas je me suis donc inscrit en BTS SIO dans le but d'apprendre les matières concernant le réseau.</p>
+<p>Après un bac STI2D EE (Énergie Environnement) obtenu avec mention, je me suis orienté vers le domaine de l'informatique. J'ai donc fait une première année de FAC en informatique, voyant qu'une partie des matières ne me correspondaient pas je me suis inscrit en BTS SIO dans le but d'apprendre les matières concernant le réseau.</p>
 
-    <p>Depuis trois ans je travaille aussi l'été. Actuellement j'ai étais dans les entreprises Cora et Leclerc. Ces expériences m'ont permis de comprendre le monde de l'entreprise et les différents modes de fonctionnement des entreprises.</p>
+    <p>Depuis trois ans je travaille aussi l'été, principalement dans les entreprises Cora et Leclerc. Ces expériences m'ont permis de comprendre le monde de l'entreprise et leurs différents modes de fonctionnement.</p>
     </div>
